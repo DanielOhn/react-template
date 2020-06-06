0e4280f14925eb999638b9ee7d6e7d9408423c17
catsby <ohndaniel@gmail.com>
2020-06-06 02:33:43 -0700

    Add font classes light/heavy with font color classes.  Add content describing purpose of site."

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
-import React from "react";
-import "../styles/App.css";
+import React from "react"
+import "../styles/App.css"
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World!</p>
+      <div className="content">
+        <h1 className="heading heavy primary">React Template</h1>
+        <p className="normal secondary">
+          This website is created for the purpose of being a template for any
+          new react projects that I create. It will come with the font weights,
+          sizes, and colors, along with npm packages that I already use for most
+          of my projects.
+        </p>
+
+        <h1 className="heading heavy primary">Other Stuff</h1>
+        <h1 className="heavy primary">Heading 1</h1>
+        <h2 className="heavy secondary">Heading 2</h2>
+        <h3 className="heavy light">Heading 3</h3>
+
+        <p className="normal secondary">
+          Normal Font for paragraphs and default content. This should be the
+          secondary content that users glance at after the headers.
+        </p>
+        <p className="normal light">
+          Minor details to content that shouldn't distract users that much
+          compared too the normal font color.
+        </p>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
